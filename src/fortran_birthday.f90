11c343b83205ea465e8a689a238a1d0bf2380134
Joe Wallwork <jw2423@cam.ac.uk>
2026-06-11 12:44:05 +0100

    Draft programme for Fortran birthday event

---
+++ src/fortran_birthday.f90
@@ -0,0 +1,29 @@
+! Programme for the 'Celebrating 70 years of Fortran' event
+program fortran_birthday
+  implicit none
+  character(len=70) :: date = "2026-12-01"
+  character(len=70) :: location = "Science and Industry Museum, Manchester"
+
+  ! Hello, World!
+  character(len=70) :: intro = "Welcome"
+  character(len=70) :: thank = "Sponsor lightning talks"
+
+  ! Past
+  character(len=70) :: keynote = "Where we've been in the past 70 years"
+
+  ! Present: Exciting developments
+  character(len=70) :: lightning1 = "TBD"
+  character(len=70) :: lightning2 = "TBD"
+  character(len=70) :: lightning3 = "TBD"
+
+  ! Present: Community initiatives
+  character(len=70) :: lightning4 = "TBD"
+  character(len=70) :: lightning5 = "TBD"
+  character(len=70) :: lightning6 = "TBD"
+
+  ! Future
+  character(len=70) :: announcement = "FortranFuture"
+  character(len=70) :: breakout = "The next 70 years"
+
+  character(len=70) :: outro = "Closing, move to reception"
+end program fortran_birthday
