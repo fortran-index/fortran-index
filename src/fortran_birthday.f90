2cd14f7de08afa9ae336f55ec74e337aa0355cf6
Joe Wallwork <jw2423@cam.ac.uk>
2026-07-03 13:59:06 +0100

    Tweak programme

--- src/fortran_birthday.f90
+++ src/fortran_birthday.f90
@@ -5,7 +5,7 @@ program fortran_birthday
   character(len=70) :: location = "Science and Industry Museum, Manchester"
 
   ! Hello, World!
-  character(len=70) :: intro = "Welcome address"
+  character(len=70) :: intro = "Welcome"
 
   ! Past
   character(len=70) :: keynote = "Where we've been in the past 70 years"
@@ -14,13 +14,9 @@ program fortran_birthday
   character(len=70) :: cake = "Cake cutting and coffee break"
   character(len=70) :: thank = "Sponsor lightning talks"
   ! Exciting developments
-  character(len=70) :: lightning1 = "TBD"
-  character(len=70) :: lightning2 = "TBD"
-  character(len=70) :: lightning3 = "TBD"
+  character(len=70) :: lightning1(3) = ["TBD", "TBD", "TBD"]
   ! Community initiatives
-  character(len=70) :: lightning4 = "TBD"
-  character(len=70) :: lightning5 = "TBD"
-  character(len=70) :: lightning6 = "TBD"
+  character(len=70) :: lightning2(3) = ["TBD", "TBD", "TBD"]
 
   ! Future
   character(len=70) :: announcement = "FortranFuture"
